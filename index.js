3cae87c5864b41354b9a73f073b1c1216d2479e0
looping-torrent-8105 <gianherrera96@gmail.com>
2020-08-06 04:56:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // write your code below!
+function happyHolidays()
+{
+  return "Happy holidays!"
+}
+
+function happyHolidaysTo(name)
+{
+  return `Happy holidays,${name}!`
+}
+
+function happyCustomHolidayTo(vacation,name)
+{
+  return `Happy ${vacation},${name}`
+}
+
+function holidayCountdown(holidayname,days) 
+{
+  return `It's ${days} days until ${holidayname}!`
+}
